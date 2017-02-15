f46861edbab5b1156960348ffb22fc13bb15a98d
Ethan Neidhart <neidhart.e@husky.neu.edu>
2017-02-15 11:35:47 -0500

    begin hw3

---
+++ public/assignment/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Ethan on 2/15/2017.
+ */
+
+(function () {
+    angular
+        .module("WebAppMaker", ["ngRoute"]);
+})();
---
+++ public/assignment/config.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Ethan on 2/15/2017.
+ */
