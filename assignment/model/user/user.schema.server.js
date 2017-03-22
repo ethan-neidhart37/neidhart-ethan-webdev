88dd35c56c2139ab9902ba29b323c8cab928e0c5
Ethan Neidhart <neidhart.e@husky.neu.edu>
2017-03-22 14:18:47 -0400

    user schema

--- assignment/model/user/user.schema.server.js
+++ assignment/model/user/user.schema.server.js
@@ -1,3 +1,19 @@
 /**
  * Created by Ethan on 3/22/2017.
  */
+
+
+module.exports = function () {
+    var mongoose = require('mongoose');
+
+    var UserSchema = mongoose.Schema({
+        username: String,
+        password: String,
+        firstName: String,
+        lastName: String,
+        email: String,
+        phone: String,
+        websites: [Website],
+        dateCreated: Date
+    }, {collection: 'assignment.user'});
+};
