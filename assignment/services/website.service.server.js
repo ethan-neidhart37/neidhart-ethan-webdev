935be3141af66ac571dc0c37e1b78bf72644c27d
Ethan Neidhart <neidhart.e@husky.neu.edu>
2017-04-19 01:47:31 -0400

    remove unnecessary arrays

--- assignment/services/website.service.server.js
+++ assignment/services/website.service.server.js
@@ -9,21 +9,12 @@ module.exports = function (app, websiteModel) {
     app.put("/api/website/:websiteId", updateWebsite);
     app.delete("/api/website/:websiteId", deleteWebsite);
 
-    var websites = [
-        {"_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem"},
-        {"_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem"},
-        {"_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem"},
-        {"_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem"},
-        {"_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem"},
-        {"_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem"}
-    ];
-
     function createWebsite(req, res) {
         var userId = req.params["userId"];
         var newWebsite = req.body;
 
         websiteModel
-            .createWebsite(userId, newWebsite)
+            .createWebsiteForUser(userId, newWebsite)
             .then(function (website) {
                 res.json(website);
             }, function (error) {
